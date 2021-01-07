871c3880b14ede9f31fb7c106cb15477e4759709
prachivishnoi <prachi27vishnoi@gmail.com>
2021-01-07 16:54:59 +0530

    context

---
+++ src/components/Button.js
@@ -0,0 +1,33 @@
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+class Button extends React.Component {
+  static contextType = LanguageContext;
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
+  render() {
+    // console.log(this.context);
+    // const text = this.context === 'english'? 'Submit': 'Voorleggen';
+    return (
+      // <button className="ui button primary" >{text}</button>
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+// static contextType = LanguageContext ==== Button.contextType = LanguageContext
+// contextType is only required when we are going to access data using this.context
+
+export default Button;
